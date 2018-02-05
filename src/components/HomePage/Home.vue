04c1fc5f527df6f95b9d845e15c217135b39df71
nishkaush <kaushish.nishant@hotmail.com>
2018-02-06 08:05:54 +1100

    killing the bugs in bidding system

--- src/components/HomePage/Home.vue
+++ src/components/HomePage/Home.vue
@@ -1,13 +1,9 @@
 <template>
     <v-layout row>
-      <v-snackbar v-model="snackbar" :top="snackBarPosition" :timeout="snackBarTimeout">
-       {{snackBarText}}
-        <v-btn class="orange" @click="snackbar=false">Close</v-btn>
-      </v-snackbar>
       <v-flex d-flex justify-content-center align-items-center v-if="showLoadingIcon===true">
         <v-progress-circular indeterminate :size="100" :width="7" color="orange">Getting Latest Listings</v-progress-circular>
       </v-flex>
-      <v-flex xs12 md8 offset-md2 v-else>
+      <v-flex xs12 md6 offset-md3 v-else>
         <div class="text-xs-right">
           <v-btn 
           class="mb-5 mt-3" 
@@ -19,10 +15,10 @@
         </div>
         <p class="title">Latest Ads for Sharing Car Rides</p>
         <v-card v-if="latestAds.length===0" class="pt-2 pb-2">Fetching Latest Listings...</v-card>
-        <v-card v-for="(item,i) in latestAds" :key="i" v-if="latestAds.length>0" class="mb-4">
+        <v-card v-for="item in latestAds" :key="item._id" v-if="latestAds.length>0" class="mb-4">
 
            <v-layout row wrap>
-             <v-flex xs12 lg7>
+             <v-flex xs12 lg9>
               <v-card-title>
                 <h3>From {{item.from.toUpperCase()}} To {{item.to.toUpperCase()}}</h3>
               </v-card-title>
@@ -30,25 +26,9 @@
                 <span class="sub-title">Travel Date {{item.travelDate}}</span>
               </v-card-text>
              </v-flex>
-             <v-flex xs12 lg5>
+             <v-flex xs12 lg3>
               <v-card-actions>
-                <v-btn @click="showSingleAd(item._id)">More Info</v-btn>
-                <v-btn @click="loginStatus?placeBidDialog=true:snackbar=true">Place a bid</v-btn>
-                <v-dialog v-model="placeBidDialog" max-width="500">
-                  <v-card class="pa-5">
-                    <v-text-field label="Enter Bid in $" v-model="bidPrice" mask="####"></v-text-field>
-                    <v-btn @click="placeNewBid(item._id,item.to,item.from)" class="orange" :loading="newBidLoadingIcon">Submit</v-btn>
-                  </v-card>
-                </v-dialog>
-                <v-btn @click="showAllBids(item._id)" :loading="allBidsLoadingIcon">Show All Bids</v-btn>
-                <v-dialog v-model="allBidsDialog" max-width="500">
-                  <v-card class="pa-4 text-xs-center">
-                    <h2 class="text-xs-center">List of Current Bids</h2>
-                    <hr>
-                    <p v-if="allBidsArr.length===0">No Bids Yet</p>
-                    <p class="mt-3" v-for="(item,i) in allBidsArr" :key="i">{{item.username}} - ${{item.bidPrice}}</p>
-                  </v-card>
-                </v-dialog>
+                <v-btn block @click="showSingleAd(item._id)">More Info</v-btn>
               </v-card-actions>
              </v-flex>
            </v-layout>
@@ -68,55 +48,18 @@
 import { setIdToken } from "./../../utils/utils";
 import gql from "graphql-tag";
 import { allBidsForAd, fetchAllAds } from "./../../Apollo/queries";
-import { placeBid } from "./../../Apollo/mutations";
 export default {
   data() {
     return {
       showLoadingIcon: false,
       refreshLoadingIcon: false,
       showMoreLoadingIcon: false,
-      newBidLoadingIcon: false,
-      allBidsLoadingIcon: false,
       showMoreBtn: true,
-      placeBidDialog: false,
-      allBidsDialog: false,
       bidPrice: "",
-      allBidsArr: [],
-      snackbar: false,
-      snackBarPosition: "top",
-      snackBarTimeout: 6000,
-      snackBarText: "Please login to place bids"
+      allBidsArr: []
     };
   },
   methods: {
-    showAllBids(adId) {
-      this.allBidsLoadingIcon = true;
-      allBidsForAd(adId)
-        .then(res => {
-          this.allBidsArr = res.data.allBidsForAd;
-          this.allBidsLoadingIcon = false;
-          this.allBidsDialog = true;
-        })
-        .catch(err => err);
-    },
-    placeNewBid(id, to, from) {
-      this.newBidLoadingIcon = true;
-      let username = this.$store.state.loggedInUser;
-      let inputArgs = { id, username, to, from, bidPrice: this.bidPrice };
-      console.log("Ad's id is -->", inputArgs.id);
-      placeBid(inputArgs)
-        .then(res => {
-          this.newBidLoadingIcon = false;
-          this.placeBidDialog = false;
-          this.snackbar = true;
-          this.snackBarText = "Your Bid Was Successfully Posted!";
-          setTimeout(
-            () => (this.snackBarText = "Please login to place bids"),
-            6000
-          );
-        })
-        .catch(err => !this.newBidLoadingIcon);
-    },
     showSingleAd(adId) {
       this.$router.push({ path: `/ad/${adId}` });
     },
